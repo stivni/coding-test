9768c5425d9a2f5c31732f9cbd1f9a7c886f4f0a
bertverstraete <bert.verstraete2@student.howest.be>
2018-03-31 19:38:37 +0200

    reformatted discountCalculator

--- src/discounts/DiscountCalculator.php
+++ src/discounts/DiscountCalculator.php
@@ -14,15 +14,6 @@ use App\Discounts\DiscountWorkers\XPctDiscountOnCheapestItemOnBuyingYProductsInZ
 
 class DiscountCalculator
 {
-
-
-    /**
-     * DiscountCalculator constructor.
-     */
-    public function __construct()
-    {
-    }
-
     public function calcDiscounts($order)
     {
 
@@ -30,40 +21,41 @@ class DiscountCalculator
         $order->totalAfterDiscounts = $order->total;
         $order->discounts = array();
 
-        /**
-         * Discount case 1:
-         * Customers that have already order for over € 1000 get a 10% discount on every order they make.
-         */
 
-        $discountWorker1 = new XPctTotalDiscountOnYAlreadyOrdered("10% total discount on order, for already buying for over €1000.",
-            1000, 10);
+        foreach ($this->getAllDiscountWorkers() as $worker) {
 
-        $discountWorker1->calcDiscount($order);
-
-
-        /**
-         * Discount case 2:
-         * For every product of category "Switches" (id 2), when you buy five, you get a sixth for free.
-         */
-
-        $discountWorker2 = new XFreeItemsWhenBuyingYFromCatZ("For every product of category \"Switches\" (id 2), when you buy five, you get a sixth for free.",
-            1, 5, 2);
-        $discountWorker2->calcDiscount($order);
-
-
-        /**
-         * Discount case 3:
-         * If you buy two or more products of category "Tools" (id 1), you get a 20% discount on the cheapest product.
-         */
-
-        $discountWorker3 = new XPctDiscountOnCheapestItemOnBuyingYProductsInZCategory("If you buy two or more products of category \"Tools\" (id 1), you get a 20% discount on the cheapest product.",
-            20, 2, 1);
-        $discountWorker3->calcDiscount($order);
+            $worker->calcDiscount($order);
+        }
 
         return $order;
 
+    }
 
+    /**
+     * Discount case 1:
+     * Customers that have already order for over € 1000 get a 10% discount on every order they make.
+     */
+    /**
+     * Discount case 2:
+     * For every product of category "Switches" (id 2), when you buy five, you get a sixth for free.
+     */
+    /**
+     * Discount case 3:
+     * If you buy two or more products of category "Tools" (id 1), you get a 20% discount on the cheapest product.
+     *
+     * Any new discounts need to be appended to the array
+     */
 
+    private function getAllDiscountWorkers()
+    {
+        return [
+            new XPctTotalDiscountOnYAlreadyOrdered("10% total discount on order, for already buying for over €1000.",
+                1000, 10),
+            new XFreeItemsWhenBuyingYFromCatZ("For every product of category \"Switches\" (id 2), when you buy five, you get a sixth for free.",
+                1, 5, 2),
+            new XPctDiscountOnCheapestItemOnBuyingYProductsInZCategory("If you buy two or more products of category \"Tools\" (id 1), you get a 20% discount on the cheapest product.",
+                20, 2, 1)
+        ];
     }
 
 
